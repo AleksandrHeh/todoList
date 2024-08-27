fe5f62a7914f8fdff6d89c6c2395180c099c1ed5
Alesandr <Aleksandr481999@yandex.ru>
2024-08-28 02:24:26 +0300

    Добавление JWT, отображение на /board email пользователя в шапке

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 	"golangify.com/snippetbox/pkg/models/pgsql"
 )
 
@@ -16,12 +19,34 @@ type user struct {
     Password 	string `json:"password"`
 }
 
-type userAuth struct {
-
-    Email    	string `json:"email"`
-    Password 	string `json:"password"`
+// Структура для хранения заявлений JWT
+type Claims struct {
+	Email string `json:"email"`
+	jwt.RegisteredClaims
 }
 
+var jwtKey = []byte("your_super_secret_key_which_is_long_and_random_enough")
+
+// Функция для создания JWT токена
+func createToken (email string) (string, error) {
+	expiratonTime := time.Now().Add(24*time.Hour)
+
+	claims := &Claims{
+		Email: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expiratonTime),
+		},
+
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenSrting, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenSrting, nil
+}
 
 func (app *application) register(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
@@ -66,7 +91,7 @@ func (app *application) register(w http.ResponseWriter, r *http.Request) {
 
 
 func (app *application) login(w http.ResponseWriter, r *http.Request) {
-    var user userAuth
+    var user user
 
     err := json.NewDecoder(r.Body).Decode(&user)
     if err != nil {
@@ -74,161 +99,40 @@ func (app *application) login(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    log.Printf("Received login request for email: %s", user.Email) // Логирование
-    
+    log.Printf("Received login request for email: %s", user.Email)
+
     exists, err := app.snippets.GetUserAuthorization(user.Email)
     if err != nil {
-        log.Printf("Error retrieving user: %v", err) // Логирование
+        log.Printf("Error retrieving user: %v", err)
         http.Error(w, "Invalid email or password", http.StatusUnauthorized)
         return
     }
 
-	hash, err := pgsql.HashPassword(user.Password)
-	if err != nil {
-		// Логируем ошибку для диагностики
-		log.Printf("Error hashing password: %v", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-    if hash != exists.Password{  
-        log.Printf("Password mismatch for email: %s", user.Email) // Логирование
+    hash, err := pgsql.HashPassword(user.Password)
+    if err != nil {
+        log.Printf("Error hashing password: %v", err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+    if hash != exists.Password {
+        log.Printf("Password mismatch for email: %s", user.Email)
         http.Error(w, "Invalid email or password", http.StatusUnauthorized)
         return
     }
-    
+
+    // Создаем JWT Token для авторизованного пользователя
+    token, err := createToken(user.Email)
+    if err != nil {
+        http.Error(w, "Could not create token", http.StatusInternalServerError)
+		log.Printf("Error signing token: %v", err) // Логирование ошибки
+        return
+    }
+
+    response := map[string]interface{}{
+        "token": token,
+        "email": user.Email,
+    }
+    w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(response)
 }
-
-
-
-/*func (app *application) showSnippetTest(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		app.notFound(w) // Страница не найдена.
-		return
-	}
-
-	// Вызываем метода Get из модели Snipping для извлечения данных для
-	// конкретной записи на основе её ID. Если подходящей записи не найдено,
-	// то возвращается ответ 404 Not Found (Страница не найдена).
-	s, err := app.snippets.Get(id)
-	if err != nil {
-		if errors.Is(err, models.ErrNoRecord) {
-			app.notFound(w)
-		} else {
-			app.serverError(w, err)
-		}
-		return
-	}
-
-	// Отображаем весь вывод на странице.
-	fmt.Fprintf(w, "%v", s)
-}
-
-
-func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-
-	s, err := app.snippets.Latest()
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
-
-}
-
-func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		app.notFound(w)
-		
-	}
-
-	key := fmt.Sprintf("snippet:%d", id)
-	var s *models.Snippet
-
-	// Попробуйте получить заметку из кэша Redis
-	cachedNote, err := app.redis.Get(key).Result()
-	if err == redis.Nil {
-		// Если заметка не найдена в кэше, получите её из базы данных
-		s, err = app.snippets.Get(id)
-		if err != nil {
-			if errors.Is(err, models.ErrNoRecord) {
-				app.notFound(w)
-			} else {
-				app.serverError(w, err)
-			}
-			return
-		}
-
-		// Сохраните заметку в кэш Redis
-		err = app.redis.Set(key, s.Content, 0).Err()
-		if err != nil {
-			app.errorLog.Printf("Ошибка кэширования заметки: %v", err)
-		}
-	} else if err != nil {
-		// Ошибка при получении заметки из Redis
-		app.serverError(w, err)
-		return
-	} else {
-		// Если данные найдены в кэше, преобразуйте их обратно в структуру Snippet
-		s = &models.Snippet{Content: cachedNote}
-	}
-
-	// Создаем экземпляр структуры templateData, содержащей данные заметки.
-	data := &templateData{Snippet: s}
-
-	files := []string{
-		"./ui/html/show.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	// Передаем структуру templateData в качестве данных для шаблона.
-	err = ts.Execute(w, data)
-	if err != nil {
-		app.serverError(w, err)
-	}
-}
-
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed) // Используем помощник clientError()
-		return
-	}
-
-	// Создаем несколько переменных, содержащих тестовые данные. Мы удалим их позже.
-	title := "История про улитку"
-	content := "Улитка выползла из раковины,\nвытянула рожки,\nи опять подобрала их."
-	expires := "7"
-
-	// Передаем данные в метод SnippetModel.Insert(), получая обратно
-	// ID только что созданной записи в базу данных.
-
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	// Перенаправляем пользователя на соответствующую страницу заметки.
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
-}
-
-func dowlandHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./ui/static/file.zip")
-}*/
